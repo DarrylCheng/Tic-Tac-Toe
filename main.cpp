9fe08ef77c4ab745489b0414c7a37d9b84ae5d89
DarrylCheng <darrylclw@gmail.com>
2016-03-16 00:30:45 +0800

    Added options to choose between 1 player (vs comp) and 2 player.

--- main.cpp
+++ main.cpp
@@ -9,53 +9,84 @@ void getInput(int&);
 
 int main(){
 	/* Tic Tac Toe Game */
-
-	//Create 2 players, X and O. True = AI, false = Player
-	Player players[2] = {Player('X',false),Player('O',true)};
-	int turn = 0;
-
-	//Create game board
-	Board board;
-	board.setPlayers(players[0],players[1]);
-
-	//initializing variables
-	int num;
-
-	while ( board.isEmpty() ){
+	while(true){
 		system("cls");
 		cout << "\tTic Tac Toe\n\n";
-		board.displayBoard();
-		cout << "\nPlayer " << players[turn] << "\'s turn. \n";
-		cout << "Enter the index number you want to input into.\n\n";
+		cout << "\t1. Play with Computer (You first)\n";
+		cout << "\t2. Play with Computer (Computer first)\n";
+		cout << "\t3. Play with your friend\n";
+		cout << "\t4. *NEW* Play with Computer with hints (You first)\n";
+		cout << "\t5. *NEW* Play with Computer with hints (Computer first)\n";
+		cout << "\n\t6. Quit\n\n";
+
+		int turn = 0;
+		bool hints = false;
+		bool player1, player2;
 		try{
-			if(board.isComputer(turn)){
-				board.computer(turn);
-			} else {
-				cout << "Suggested moves: ";
-				board.suggestedMoves(turn);
-				getInput(num);
-				if (num == 911){
-					return 0;
-				}
-				board.insert(num, turn);
+		int input;
+			getInput(input);
+			switch(input){
+				//True = AI, False = Player
+				case 1: player1 = false; player2 = true; break;
+				case 2: player1 = true; player2 = false; break;
+				case 3: player1 = false; player2 = false; break;
+				case 4: player1 = false; player2 = true; hints = true; break;
+				case 5: player1 = true; player2 = false; hints = true; break;
+				case 6: return 0;
 			}
+			if(input <=0 || input >=7)
+				throw Exceptions("Input out of range.");
 		} catch ( Exceptions e ){
 			cout << endl << e.getMsg() << endl;
 			system("pause");
 			continue;
 		}
-		if( board.winConditionsMet(board.getBoard()) )
-			break;
-		turn = (turn + 1) % 2;
-	}
 
-	system("cls");
-	cout << "\tTic Tac Toe\n\n";
-	board.displayBoard();
-	if(board.winConditionsMet(board.getBoard())){
-		cout << "\nThe winner is " << players[turn];
-	} else {
-		cout << "\nA draw game!";
+		Player players[2] = {Player('X',player1),Player('O',player2)};
+
+		//Create game board
+		Board board;
+		board.setPlayers(players[0],players[1]);
+
+		while ( board.isEmpty() ){
+			int num;
+			system("cls");
+			cout << "\tTic Tac Toe\n\n";
+			board.displayBoard();
+			cout << "\nPlayer " << players[turn] << "\'s turn. \n";
+			cout << "Enter the index number you want to input into.\n\n";
+			try{
+				if(board.isComputer(turn)){
+					board.computer(turn);
+				} else {
+					if(hints){
+						cout << "Suggested moves: ";
+						board.suggestedMoves(turn);
+					}
+					getInput(num);
+					board.insert(num, turn);
+				}
+			} catch ( Exceptions e ){
+				cout << endl << e.getMsg() << endl;
+				system("pause");
+				continue;
+			}
+			if( board.winConditionsMet(board.getBoard()) )
+				break;
+			turn = (turn + 1) % 2;
+		}
+
+		system("cls");
+		cout << "\tTic Tac Toe\n\n";
+		board.displayBoard();
+		if(board.winConditionsMet(board.getBoard())){
+			if(board.isComputer(turn))
+				cout << "\nThe computer won and you lost.";
+			cout << "\nThe winner is " << players[turn] << endl;
+		} else {
+			cout << "\nA draw game!\n";
+		}
+		system("pause");
 	}
 	
 }
@@ -65,7 +96,7 @@ void getInput(int& num){
 	cin.sync();
 	cout << "Enter number : ";
 	cin >> raw_num;
-	if( !(istringstream(raw_num) >> num ) ){
+	if( !(istringstream(raw_num) >> num ) || raw_num.length()>1){
 		throw Exceptions("Error: Non-integer input detected.");
 	}
 }
